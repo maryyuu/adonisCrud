de47759fa28d332ff5af10f058910a93d559c4f4
maryyuu <charigdlopez@gmail.com>
2024-05-29 11:43:54 -0500

    Problema al ejecutar los migrations: run Archivo daatabase, config, y base de datos al profe

--- app/models/animales_models.ts
+++ app/models/animales_models.ts
@@ -1,7 +1,37 @@
-export interface Animal {
-    id:number,
-    name: string,
-    color : string,
-    vista: string,
-    extincion: string
+import { DateTime } from "luxon";
+import { BaseModel, column } from "@adonisjs/lucid/orm";
+
+export default class Animales extends BaseModel{
+  @column ({isPrimary : true})
+  declare id: number
+
+  @column()
+  declare animalName: string
+
+  @column()
+  declare color: string
+
+  @column()
+  declare vista: string
+
+  @column()
+  declare extincion: string
+
+
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+
 }
+// export interface Animal {
+//     id:number,
+//     name: string,
+//     color : string,
+//     vista: string,
+//     extincion: string
+// }
+
